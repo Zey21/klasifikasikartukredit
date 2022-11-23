777031a2e243af726d0ccdcbbc4a2f66768da0b4
Zey21 <77796843+Zey21@users.noreply.github.com>
2022-11-24 02:38:07 +0700

    Edit Config
    Add route html

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+import os
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/',methods = ['POST','GET'])
+def web():
+    return render_template('index.html')
+
+@app.route('/home',methods = ['POST','GET'])
+def web1():
+    return render_template('index.html')
+
+@app.route('/fitur',methods = ['POST','GET'])
+def web2():
+    return render_template('sub.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
